9ede2e4ecb23d31761ab06ee26b799f9fbd99785
jjmarks <jjmarks@pm.me>
2021-03-29 22:46:44 +0100

    add normal cdf function

--- BSPE.f95
+++ BSPE.f95
@@ -2,7 +2,7 @@ program BSPE
    implicit none
    ! BS Variables & Input
    real :: S_0, K, T, sigma, r
-   real :: BS_CALCULATE
+   real :: BS_CALCULATE, Phi
 
    print*, "Input order: stock price, strike price, time to maturity, voltality, interest rate."
 
@@ -16,9 +16,22 @@ end program BSPE
 function BS_CALCULATE(S_0, K, T, sigma, r)
    implicit none
    real :: S_0, K, T, sigma, r, omega
-   real :: BS_CALCULATE, ANORDF
+   real :: BS_CALCULATE, Phi
 
    omega = ( r*T + (sigma**2 * T / 2) - log(K/S_0) ) / ( sigma * sqrt(T) )
    
-   !BS_CALCULATE = S_0 * Phi(omega) - K * exp(-r*T) * Phi(omega - sigma*sqrt(T))
+   BS_CALCULATE = S_0 * Phi(omega) - K * exp(-r*T) * Phi(omega - sigma*sqrt(T))
 end function BS_CALCULATE
+
+function Phi(z)
+   implicit none
+   real, parameter :: pi = 4.D0*DATAN(1.D0)
+   real :: Phi, z
+   integer :: k
+
+   ! Taylor series normal cdf approximation
+   Phi = 0.5
+   do k = 0, 20
+      Phi = Phi + ( ((-1.0)**k) * (z** (2.0*k+1.0)) ) / ( (2.0**k) * gamma(real(k+1)) * (2.0*k+1.0) ) * (1.0/sqrt(2*pi))
+   end do
+end function Phi
